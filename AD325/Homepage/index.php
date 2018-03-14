6aa5f75a0dd218a3add1acee1bae5c23fab834d5
DM Coulter <dcoult01@seattlecentral.edu>
2018-03-14 12:57:27 -0700

    Create index.php
    Staging area for AD 325 Web Applications Development class at North Central College, Winter 2018.

---
+++ AD325/Homepage/index.php
@@ -0,0 +1,109 @@
+<html>
+<!--
+ * Class:       AD 325 WI 18
+ * Author:      Danielle Coulter
+ * Assignment:  Lab 1
+ * Title:       index.php 
+ * Due Date:    March 7, 2018
+-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <title>Coulter AD 325 Home</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+
+    <style>
+    .container {
+    margin: 0 auto;
+    width: 500px;
+    }
+    </style>
+        
+        
+</head>
+
+    <body>
+        <div class="container">
+            <div class="column">
+            <h1 class='align-center'>AD 325 Web App Dev</h1>
+            <h2 class='align-center'>Danielle Coulter Stage</h2>
+           
+            <br>
+            <h4>Lab Work</h4>
+            <a href="index.php"><input type="" class="btn btn-primary" value="Lab 1" /></a>
+            <br>
+            <br>
+            <a href="lab2/index.html"><input type="" class="btn btn-primary" value="Lab 1" /></a>
+            <br>
+            <br>
+            
+            <a href="lab3/lab3.1/index.html"><input type="" class="btn btn-primary" value="Lab 3.1" /></a>
+            <br>
+            <br>
+                <a href="lab3/lab3.2/"><input type="" class="btn btn-primary" value="Lab 3.2" /></a>
+            <br>
+            <br>
+                <a href="lab4/4.1/index.php"><input type="" class="btn btn-primary" value="Lab 4.1" /></a>
+            <br>
+            <br>
+                <a href="lab4/4.2/index.php"><input type="" class="btn btn-primary" value="Lab 4.2" /></a>
+            <br>
+            <br>
+                <a href="lab4/4.3/index.php"><input type="" class="btn btn-primary" value="Lab 4.3" /></a>
+            <br>
+            <br>
+                <a href="lab5/index.php"><input type="" class="btn btn-primary" value="Lab 5" /></a>
+            <br>
+            <br>
+                <a href="lab7/index.php"><input type="" class="btn btn-primary" value="Lab 6" /></a>
+            <br>
+            <br>
+                <a href="l7/validation.php"><input type="" class="btn btn-primary" value="Lab 7" /></a>
+            <br>
+            <br>
+                <h4>. . .</h4>            
+            <br>
+                <a href="https://canvas.northseattle.edu/api/v1/courses?access_token=9~utRdSLfAFMzcOvTuhMFwjqZElaYHnDjwilyA06lKpTE5bHLB7hKmfetLgxDiTjuS"><input type="" class="btn btn-primary" value="Lab 10" /></a>
+            <br>
+            <br>
+                
+                <h4>Homework</h4>
+                <a href="assignment1/ch01_product_discount/index.html"><input type="" class="btn btn-primary" value="Assignment 1" /></a>
+            <br>
+            <br>
+                <a href="assignment2/LoanCal.php"><input type="" class="btn btn-primary" value="Assignment 2" /></a>
+            <br>
+            <br>
+                <a href="assignment3/index.php"><input type="" class="btn btn-primary" value="Assignment 3" /></a>
+            <br>
+            <br>
+                <a href="assignment4/index.php"><input type="" class="btn btn-primary" value="Assignment 4" /></a>
+            <br>
+            <br>
+                <a href="lab5/index.php"><input type="" class="btn btn-primary" value="Assignment 5" /></a>
+            <br>
+            <br>
+                <a href="a6/pdo/"><input type="" class="btn btn-primary" value="Assignment 6" /></a>
+            <br>
+            <br>
+                <a href="a7/index.php"><input type="" class="btn btn-primary" value="Assignment 7" /></a>
+            <br>
+            <br>
+                <a href="a8/index.html"><input type="" class="btn btn-primary" value="Assignment 8" /></a>
+            <br>
+            <br>
+                <a href="assignment9/assignment9.html"><input type="" class="btn btn-primary" value="Assignment 9" /></a>
+            <br>
+            <br>
+                <a href="a10/index.html"><input type="" class="btn btn-primary" value="Assignment 10" /></a>
+            <br>
+            <br>
+                
+
+            </div> 
+          </div>
+    </body>
+</html>
